65e19c60a83f718fbdf85eb28f5398a78902f3ca
Nik Sumeiko <nik.sumeiko@gmail.com>
2025-08-28 14:06:49 +0200

    test: Create address model validation

---
+++ src/addresses/useCases/__tests__/createAddressModel.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it } from 'vitest';
+import { createAddressModel, Fields } from '../createAddressModel';
+
+describe('Create address view model', () => {
+    describe('model input validation', () => {
+        it('passes when optional field is provided', () => {
+            const fields = {
+                street: { requirement: 'OPTIONAL', label: 'street' },
+            } satisfies Fields;
+            const model = createAddressModel(fields);
+            const formValues = { street: 'x' };
+
+            const result = model.validate(formValues);
+
+            expect(result).toEqual(formValues);
+        });
+
+        it('passes when required field is provided', () => {
+            const fields = {
+                street: { requirement: 'MANDATORY', label: 'street' },
+            } satisfies Fields;
+            const model = createAddressModel(fields);
+            const formValues = { street: 'x' };
+
+            const result = model.validate(formValues);
+
+            expect(result).toEqual(formValues);
+        });
+
+        it('passes when required field is empty', () => {
+            const fields = {
+                street: { requirement: 'MANDATORY', label: 'street' },
+            } satisfies Fields;
+            const model = createAddressModel(fields);
+            const formValues = { street: '' };
+
+            expect(() => model.validate(formValues)).toThrowError('street is missing.');
+        });
+    });
+});
